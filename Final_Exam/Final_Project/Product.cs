c0c986f2d319c234056d1cdc1c7a93ca17cf9e13
phuocding <phuocding@gmail.com>
2018-06-26 20:53:18 +0700

    Nguyen Dinh Phuoc Final

---
+++ Final_Exam/Final_Project/Product.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Final
+{
+    public class Product
+    {
+        private string _productId;
+        private string _name;
+        private double _price;
+
+        public Product()
+        {
+        }
+
+        public Product(string productId, string name, double price)
+        {
+            _productId = productId;
+            _name = name;
+            _price = price;
+        }
+
+        public string ProductName
+        {
+            get => _productId;
+            set => _name = value;
+        }
+
+        public string ProductId
+        {
+            get => _productId;
+            set => _productId = value;
+        }
+
+        public double Price
+        {
+            get => _price;
+            set => _price = value;
+        }
+    }
+}
